611078570a800c713cb16782d0b99a0ce7a5fbf2
Jake Shamash <jake.shamash@airbnb.com>
2019-12-20 02:14:55 -0800

    Extract MIDI constants

--- src/main/java/sm808/outputdevices/MidiOutputDevice.java
+++ src/main/java/sm808/outputdevices/MidiOutputDevice.java
@@ -18,22 +18,37 @@ public class MidiOutputDevice implements OutputDevice {
           .put(Event.HIHAT, 62)
           .build();
 
+  private final Map<Event, ShortMessage> midiMessages;
+
+  public MidiOutputDevice() {
+    ImmutableMap.Builder<Event, ShortMessage> builder = ImmutableMap.builder();
+
+    for (Map.Entry<Event, Integer> entry : MIDI_MAPPING.entrySet()) {
+      try {
+        builder.put(
+            entry.getKey(), new ShortMessage(ShortMessage.NOTE_ON, 0, entry.getValue(), VELOCITY));
+      } catch (InvalidMidiDataException e) {
+        // We won't be able to play this event since its MIDI mapping is poorly configured.
+        e.printStackTrace();
+      }
+    }
+    midiMessages = builder.build();
+  }
+
   @Override
   public void play(Set<Event> events) throws PlaybackException {
     for (Event event : events) {
       try {
         play(event);
-      } catch (InvalidMidiDataException | MidiUnavailableException e) {
+      } catch (MidiUnavailableException e) {
         throw new PlaybackException(e);
       }
     }
   }
 
-  private void play(Event event) throws InvalidMidiDataException, MidiUnavailableException {
-    // Picks an arbitrary default value if no value defined.
-    int noteValue = MIDI_MAPPING.getOrDefault(event, 0);
-    ShortMessage message = new ShortMessage();
-    message.setMessage(ShortMessage.NOTE_ON, 0, noteValue, VELOCITY);
+  private void play(Event event) throws MidiUnavailableException {
+    ShortMessage message = midiMessages.get(event);
+    if (message == null) return;
     // Sends to the default receiver. An improvement could be to supply the Receiver on class
     // construction.
     MidiSystem.getReceiver().send(message, -1);
